bddeda931bc100064ff8746066b5b9fa57be28e7
hundov <hundovjob@gmail.com>
2026-09-16 21:30:33 +0300

    init commit

---
+++ src/client/java/main/client/mainClient.java
@@ -0,0 +1,10 @@
+package main.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class mainClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
